efd1691590f6bc9c66d52da3093fd72a70539a9b
Brayan Olivares <bryan151972@hotmail.com>
2024-11-29 12:49:18 -0600

    andriod

---
+++ iti-271311_eq_08_u3_latex/Z_ITI-271311_U3_E08/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Z_ITI-271311_U3_E08"
+include(":app")
+ 
